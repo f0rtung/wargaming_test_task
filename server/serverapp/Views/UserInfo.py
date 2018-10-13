976c3f25ab0a9ad11d01fe6a842dd72ff0c1ed0e
f0rtung <zadov.vasya@gmail.com>
2018-10-13 21:30:57 +0300

    Add UserInfo view

---
+++ server/serverapp/Views/UserInfo.py
@@ -0,0 +1,15 @@
+from ..Models.UserItemModel import user_items
+from ..Utils.Decorators import json_response
+from ..Models.UsersModel import get_existing_user
+from ..Utils.RequestWrapper import RequestWrapper
+
+from ..Responses.UserWithItemsResponse import UserWithItemsResponse
+
+
+@json_response
+def user_info(request):
+    rw = RequestWrapper(request)
+    user_id = rw.user_id()
+    user = get_existing_user(user_id)
+    items = user_items(user)
+    return UserWithItemsResponse(user, items)
--- server/serverapp/urls.py
+++ server/serverapp/urls.py
@@ -4,10 +4,12 @@ from serverapp.Views import LoginView
 from serverapp.Views import ItemsView
 from serverapp.Views import SellView
 from serverapp.Views import BuyView
+from serverapp.Views import UserInfo
 
 urlpatterns = [
     url(r'^login/$', LoginView.login),
     url(r'^items/$', ItemsView.items),
     url(r'^sell/$', SellView.sell),
     url(r'^buy/$', BuyView.buy),
+    url(r'^user_info$', UserInfo.user_info),
 ]
