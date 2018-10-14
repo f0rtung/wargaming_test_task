f21286a50ecb6bc00dce8b491b265354790f8332
f0rtung <zadov.vasya@gmail.com>
2018-10-14 13:13:54 +0300

    Add api_url as param

--- client/main.py
+++ client/main.py
@@ -1,5 +1,14 @@
-from StateMachine import StateMachine
+import argparse
+from StateMachine.StateMachine import StateMachine
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_url", help="base apu url", default="http://127.0.0.1:8000/server")
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-    sm = StateMachine('http://127.0.0.1:8000/server')
+    args = parse_args()
+    sm = StateMachine(args.api_url)
     sm.run()
--- run_client.sh
+++ run_client.sh
@@ -1,3 +1,3 @@
 source myvenv/bin/activate
 cd client
-python3 main.py
+python3 main.py --api_url "http://127.0.0.1:8000/server"
